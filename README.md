bb94f6a1c10df813cf5dbdd7a95dc11af006cd8f
gct38 <gordontsang1@gmail.com>
2020-03-30 22:05:23 -0400

    Granted authentication to Heroku Postgresql database

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 Use Exposed Library from JetBrains. Used to connect to PostgreSQL database that's currently hosted on Heroku.
-    https://github.com/JetBrains/Exposed
 
-    It currently supports PostgreSQL, MySQL, MariaDB, Oracle, SQLite, H2, SQL Server
+val db = Database.connect("jdbc:postgresql://ec2-23-21-225-251.compute-1.amazonaws.com:5432/d4tefl2m5ii7b7?sslmode=require&user=lruthoqamfsgna&password=5490aebc5c8ccdd3a97f0c287d00e55f3d702b4413d8ee1802a967d6c8706f10")
+Form of Database connection string: jdbc:postgresql://<host>:<port>/<databasename>?sslmode=require&user=<username>&password=<password>
+NEED: (all of which can be gotten from the Heroku database in Settings under Database Credentials)
+    - host
+    - port
+    - database name
+    - usename
+    - password
 
-    Need to work on authentication. Need to somehow bridge Heroku security and Android Studio gradle.
-    https://devcenter.heroku.com/articles/getting-started-with-gradle-on-heroku#declare-app-dependencies
-    https://devcenter.heroku.com/articles/connecting-to-relational-databases-on-heroku-with-java
-    Or just whitelist all IP addresses
+https://github.com/JetBrains/Exposed
+
+It currently supports PostgreSQL, MySQL, MariaDB, Oracle, SQLite, H2, SQL Server
+
+Need to work on authentication. Need to somehow bridge Heroku security and Android Studio gradle.
+https://devcenter.heroku.com/articles/getting-started-with-gradle-on-heroku#declare-app-dependencies
+https://devcenter.heroku.com/articles/connecting-to-relational-databases-on-heroku-with-java
+
 
 Make cure connectionlayer class is Async task or it will not work
+
+
+
--- app/src/main/java/com/example/aisports/data/LoginDataSource.kt
+++ app/src/main/java/com/example/aisports/data/LoginDataSource.kt
@@ -1,21 +1,50 @@
 package com.example.aisports.data
 
 import android.annotation.SuppressLint
+import android.os.AsyncTask
+import android.util.Log
 import com.example.aisports.data.model.LoggedInUser
-import com.example.aisports.ui.login.LoggedInUserView
-import com.example.aisports.ui.login.LoginResult
-import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.jodatime.datetime
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.DateTime
 import java.io.IOException
-import java.util.*
 
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 
+object Users: Table("auth_user") {
+    val id = integer("id")
+    val password = varchar("password", 128)
+    val last_login = datetime("last_login").clientDefault{ DateTime.now() }
+    val is_superuser = bool("is_superuser")
+    val username = varchar("username", length = 50)
+    val first_name = varchar("first_name", 30)
+    val last_name = varchar("last_name", 150)
+    val email = varchar("email", 254)
+    val is_staff = bool("is_staff")
+    val is_active = integer("is_active")
+    val date_joined = datetime("date_joined").clientDefault{ DateTime.now() }
+}
+
+class backgroundTask() : AsyncTask<Void, Void, String>() {
+    override fun doInBackground(vararg params: Void?): String? {
+        val db = Database.connect("jdbc:postgresql://ec2-23-21-225-251.compute-1.amazonaws.com:5432/d4tefl2m5ii7b7?sslmode=require&user=lruthoqamfsgna&password=5490aebc5c8ccdd3a97f0c287d00e55f3d702b4413d8ee1802a967d6c8706f10")
+
+        val name = "YOU SUCK " + db.name
+        Log.d("DATABASE CONNECTION",name)
+
+        val returnValue = transaction {
+            Users.select{ Users.username eq "test" }.toList()
+            //List is of length 1 (one)
+        }
+        //val fakeUser = LoggedInUser(returnValue[0].toString(), returnValue[4].toString())
+        Log.d("DATABASE CONNECTION", returnValue[0].toString())
+        return returnValue[0].toString()
+    }
+}
 
 
 class LoginDataSource {
@@ -25,16 +54,11 @@ class LoginDataSource {
         try {
             // TODO: handle loggedInUser authentication
 
-            val db = Database.connect("jdbc:postgresql://lruthoqamfsgna:5490aebc5c8ccdd3a97f0c287d00e55f3d702b4413d8ee1802a967d6c8706f10@ec2-23-21-225-251.compute-1.amazonaws.com:5432/d4tefl2m5ii7b7", driver = "org.postgresql.Driver")
-
-            /*
-            val returnValue = transaction {
-                auth_user.select{ auth_user.username = "test" }.toList()
-            }
-            val fakeUser = LoggedInUser(returnValue[0], returnValue[4])
-            */
-
-            val fakeUser = LoggedInUser(UUID.randomUUID().toString(), "Jane Doe")
+            backgroundTask().execute()
+            val fakeUser = LoggedInUser("dsfkljf", "JANE DOE")
+            //val userDetails: String = backgroundTask().execute()
+            //val userInfo = userDetails.split(" ".toRegex()).toList()
+            //val fakeUser = LoggedInUser(userInfo[0], userInfo[1])
             return Result.Success(fakeUser)
         } catch (e: Throwable) {
             return Result.Error(IOException("Error logging in", e))
